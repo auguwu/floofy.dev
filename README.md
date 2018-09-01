2cf0bbfa6322805109db7f39274a687ba5f9d6a9
August (Chris) <augu@voided.pw>
2018-08-31 23:51:43 -0700

    meta: Nuke the repo so it can show the new layout.

---
+++ README.md
@@ -0,0 +1,2 @@
+# augu.me [![Discord](https://discordapp.com/api/guilds/382725233695522816/embed.png)](https://discord.gg/7TtMP2n)
+`augu.me` is my Website's source code.
---
+++ src/Website.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+app.disable('x-powered-by');
+const path = require('path');
+let options = {
+  PROTOCOL: 'http',
+  DEV: true
+};
+
+process.seed = Math.ceil(Math.random() * Number.MAX_SAFE_INTEGER);
+
+app
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('index.ejs'))
+  .listen(3000, () => console.log(`[Seed ${process.seed}] [Process ${process.pid}] >> ${options.PROTOCOL === 'http' ? 'http://' : "https://"}${options.dev === false ? 'augu.me' : "localhost:3000"} is now live.`));
