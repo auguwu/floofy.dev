7f0b399942a88839655078993f5b95e33285b3af
August (Chris) <augu@voided.pw>
2019-10-09 03:35:09 -0700

    Remove Inuyama thing, add reference photo for something

--- src/routes/main.js
+++ src/routes/main.js
@@ -2,7 +2,7 @@ const { Router } = require('express');
 const router = Router();
 
 router.get('/', (_, res) => res.render('index.ejs'));
-router.get('/discord', (_, res) => res.redirect('https://discord.gg/yDnbEDH'));
+router.get('/discord', (_, res) => res.redirect('https://discord.gg/jhWNPtH'));
 router.get('/github', (_, res) => res.redirect('https://github.com/auguwu'));
 
 module.exports = router;
