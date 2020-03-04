1f20211c991b5b03b93bd34eccfbfbccd2375eff
Noel <augu@voided.pw>
2020-03-03 21:05:46 -0700

    Calculate fullstacking years and age correctly

--- src/js/birthday.js
+++ src/js/birthday.js
@@ -14,11 +14,13 @@ if (d.getMonth() === 2 && d.getDate() === 24) {
   console.log('[Birthday] It is not March 24th.');
 }
 
-function testConfetti() {
+function testConfetti(timeout = 3000) {
+  const time = timeout < 1000 ? timeout * 1000 : timeout;
+
   console.log('[Birthday] User wanted to test confetti');
   confetti.start();
   setTimeout(() => {
     console.log('[Birthday] Stopping confetti');
     confetti.stop();
-  }, 2000);
+  }, time);
 }
