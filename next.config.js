d1d7083ce57eb49443aaf7a9eaf59049d554d24e
Noel <cutie@floofy.dev>
2021-07-04 06:23:16 -0700

    Fix builds

--- next.config.js
+++ next.config.js
@@ -2,10 +2,12 @@ module.exports = {
   productionBrowserSourceMaps: true,
   poweredByHeader: false,
   trailingSlash: true,
-  future: {
-    webpack5: true
-  },
+  webpack5: true,
   images: {
     domains: ['cdn.floofy.dev']
+  },
+  eslint: {
+    // It is already linting in workflow, so this isn't needed
+    ignoreDuringBuilds: true
   }
 };
